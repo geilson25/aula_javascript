a48d0ac88b3a143ade8f2e69c9a7ee8240917177
Geilson <geilsonfidelis@gmail.com>
2020-06-04 10:03:07 -0300

    Aula 02: console.log; .push; .pop

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
+var frutas = [{nome:"Maçã", cor:"vermelha"}, {nome:"Uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
 
-var nome = "Fidelis, Geilson";
+
+/*var fruta = {nome: "Maçã", cor: "vermelho"};
+console.log(fruta.nome);*/
+
+
+
+//var lista = ["Maçã", "Pera", "Laranja"];
+//lista.push("Uva");
+//lista.pop("Laranja");
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//onsole.log(lista.toString()[0]);
+//console.log(lista.join(" - "));
+
+
+/*var nome = "Fidelis, Geilson";
 var td = "∴"
 var frase = "Spes messis in Semine";
 alert("Bem vindo ao Blog do " + td + nome + td);
 console.log(nome);
 console.log(td+nome+td);
-console.log(frase);
+console.log(frase);*/
