8bcedb4f47d7994365e3a9ef6a3d9883eb1f3d42
Geilson <geilsonfidelis@gmail.com>
2020-06-04 01:50:01 -0300

    aula 01: var;alert; console

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var nome = "Fidelis, Geilson";
+var td = "∴"
+var frase = "Spes messis in Semine";
+alert("Bem vindo ao Blog do " + td + nome + td);
+console.log(nome);
+console.log(td+nome+td);
+console.log(frase);
